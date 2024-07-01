537a59add82935cd9300f135d4871dba9b6b6a26
Galley Hugo <hga@unif.local>
2024-07-01 15:18:49 +0200

    Finalisations de la page Main

--- src/Composants/App.js
+++ src/Composants/App.js
@@ -4,6 +4,8 @@ import React from 'react';
 import NavBar from './NavBar';
 import BarStory from './BarreStory'
 import CardPost from './CardPost';
+import FicheContact from './FicheContact';
+import test from '../items/99678890.jpeg'
 
 function App() {
   return (
@@ -25,7 +27,14 @@ function App() {
             </div>
           </div>
           <div className='InfoCompte'>
-
+            <FicheContact key={1} username={"hugigogo_"} pseudo={"SuperBg"} profilePicture={test} />
+            <p className='SugForYou'>Suggestions pour vous</p>
+            <FicheContact key={1} username={"hugigogo_"} pseudo={"SuperBg"} profilePicture={test} />
+            <FicheContact key={1} username={"hugigogo_"} pseudo={"SuperBg"} profilePicture={test} />
+            <FicheContact key={1} username={"hugigogo_"} pseudo={"SuperBg"} profilePicture={test} />
+            <FicheContact key={1} username={"hugigogo_"} pseudo={"SuperBg"} profilePicture={test} />
+            <p className='LegalCredits'>À propos Aide Presse API Emplois
+            <br/>Confidentialité Conditions Lieux Langue <br/>Meta Verified</p>
           </div>
         </div>
       </div>
---
+++ src/Composants/FicheContact.js
@@ -0,0 +1,14 @@
+import '../Styles/FicheContact.css'
+
+export default function FicheContact({username,pseudo,profilePicture}){
+    return (
+        <div className="FicheContact">
+            <img src={profilePicture} alt="profilePicture"/>
+            <div className="info-name">
+                <p>{username}</p>
+                <p>{pseudo}</p>
+            </div>
+            <p className='Suivre'>Suivre</p>
+        </div>
+    )
+}
