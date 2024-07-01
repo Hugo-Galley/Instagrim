d9d946df77e3824ded1c8559ce558adf6c108b96
Galley Hugo <hga@unif.local>
2024-07-01 16:01:05 +0200

    Ajout des Likes dynamique

--- src/Composants/CardPost.js
+++ src/Composants/CardPost.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import '../Styles/CardPost.css'
 import logoTest from '../items/99678890.jpeg'
 import contentPng from '../items/content.jpg'
@@ -13,49 +13,48 @@ function EnteteCard({username,profilePicture}){
     )
 }
 
-function DescriptionCard({nbrLike,nbrComment,description,timeStampPost}){
-    // const now = new Date();
-    // let tempsDepuisPost = (now.getHours() - new Date()) / 60
-    // let TimeEcoule = "Il y'a " + tempsDepuisPost + " heures"
-    // if (tempsDepuisPost <=1){
-    //     tempsDepuisPost /= 60
-         let TimeEcoule = "Il y'a " + 3 + " minutes"
-         const [like, setLike] = useState(false);
-         const [likesCount, setLikesCount] = useState(nbrLike);
-     
-         function AddLike() {
-             setLikesCount(likesCount + 1);
-             setLike(true);
-         }
-     
-         function RemoveLike() {
-             setLikesCount(likesCount - 1);
-             setLike(false);
-         }
-         
-    return(
+function DescriptionCard({ initialNbrLike, nbrComment, description, timeStampPost }) {
+    const [nbrLike, setNbrLike] = useState(() => {
+        const parsedLike = parseInt(initialNbrLike, 10);
+        return isNaN(parsedLike) ? 0 : parsedLike;
+    });
+    const [userLiked, setUserLiked] = useState(false);
+    
+    const TimeEcoule = "Il y'a 3 minutes";
+
+    function toggleLike() {
+        if (userLiked) {
+            setNbrLike(prevNbrLike => prevNbrLike - 1);
+        } else {
+            setNbrLike(prevNbrLike => prevNbrLike + 1);
+        }
+        setUserLiked(prevLiked => !prevLiked);
+    }
+
+    // Reset nbrLike if initialNbrLike changes
+    useEffect(() => {
+        const parsedLike = parseInt(initialNbrLike, 10);
+        setNbrLike(isNaN(parsedLike) ? 0 : parsedLike);
+        setUserLiked(false);
+    }, [initialNbrLike]);
+
+    return (
         <div className='DescriptionCard'>
             <div className='IconDesciption'>
-            <ul>
-                
-            <li>
-                    {like ? (
-                        
-                        <button className='LikeButton' onClick={AddLike}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-                            </svg>
+                <ul>
+                    <li>
+                        <button className='LikeButton' onClick={toggleLike}>
+                            {userLiked ? (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                                </svg>
+                            ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
+                                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                                </svg>
+                            )}
                         </button>
-                        
-                    ) : (
-                        <button className='LikeButton' onClick={RemoveLike}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-                            </svg>
-                        </button>
-                        
-                    )}
-                </li>
+                    </li>
 
                 <li>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
