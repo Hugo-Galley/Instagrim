3443fd4303892cba169457081e2e214d533b3dc1
Galley Hugo <hga@unif.local>
2024-07-01 14:40:10 +0200

    Mise au propre des Stories et de leurs scroll

--- src/Composants/BarreStory.js
+++ src/Composants/BarreStory.js
@@ -1,29 +1,35 @@
-import React, { useRef } from 'react';
-import '../Styles/BarreStory.css'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useRef, useState } from 'react';
 import pp1 from '../items/99678890.jpeg';
 import pp2 from '../items/instaLogo.svg';
 import pp3 from '../items/instaLogoWhite.svg';
 import Story from './Storys';
+import '../Styles/BarreStory.css'
 
-function BarStory() {
-    const carouselRef = useRef(null);
-
-    let listePP = [pp1, pp2, pp3];
-    let listeUsername = ['Hugo', 'Tata', 'Chef', "user2", "user5", "user6", "user7","userr1","ERTYUJ"];
-
+export default function BarStory() {
+    const scrollRef = useRef(null);
+  
+    const scroll = (direction) => {
+      const container = scrollRef.current;
+      const scrollAmount = direction === 'left' ? -120 : 120;
+      container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    };
+  
+    const listePP = ['/api/placeholder/64/64', '/api/placeholder/64/64', '/api/placeholder/64/64'];
+    const listeUsername = ['Hugo', 'Tata', 'Chef', "user2", "user5", "user6", "user7", "userr1", "ERTYUJ"];
+  
     return (
-        <div className="BarStory">
-            <div className="carousel-container">
-                <div className="carousel" ref={carouselRef}>
-                    {listeUsername.map((username, i) => (
-                        <Story key={i} username={username} profilPhoto={listePP[i]} />
-                    ))}
-                </div>
-            </div>
+      <div className="bar-story">
+        <button onClick={() => scroll('left')} className="scroll-button left">
+          &lt;
+        </button>
+        <div ref={scrollRef} className="story-container">
+          {listeUsername.map((username, i) => (
+            <Story key={i} username={username} profilPhoto={pp1} />
+          ))}
         </div>
+        <button onClick={() => scroll('right')} className="scroll-button right">
+          &gt;
+        </button>
+      </div>
     );
-}
-
-export default BarStory;
+  }
