6988925f8f3d7c7b62ec17793870262a704741b2
Freaks <galleyhugo@icloud.com>
2024-07-14 11:12:01 +0200

    Test basroot

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './Styles/App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import NavBar from './Composants/NavBar'; 
 import Profile from './Pages/Profil';
@@ -8,15 +8,16 @@ import Reel from './Pages/Reels';
 
 function App() {
   return (
-    <Router>
+
+    <BrowserRouter basename='/Instagrim'>
           <NavBar />
       <Routes>
-        <Route path='/Instagrim' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/profil' element={<Profile />} />
         <Route path='/discover' element={<Dicover />} />
         <Route path='/reels' element={<Reel/>} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 export default App
